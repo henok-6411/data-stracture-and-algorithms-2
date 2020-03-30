51bb36f78681f4e075a198393a85b3f1dc8b38b6
henok Gebremedhn <henoklwi@gmail.com>
2020-03-30 02:41:05 -0700

    linked-list half work

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Node {
-  constructor(n) {
-
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+    this.prev = null;
   }
 }
 
 
 
 class LinkedList {
-  constructor(n) {
+  constructor(value) {
+    this.head = value;
+  }
 
+  insert(val) {
+    let newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+  includes(val) {
+    if (this.head === val) {
+      false;
+    } else true;
+  }
+  toString() {
+    while (this.head) {
+      console.log('{' + this.head + '}' + '->' + '{' + this.head + '}' + '->' + '{' + this.head + '}' + '->' + 'NULL');
+      this.head = this.head.next;
+    }
   }
 }
+
+let myNode = new LinkedList(3);
+
+myNode.insert('A');
+myNode.insert('B');
+myNode.insert('C');
+myNode.toString();
