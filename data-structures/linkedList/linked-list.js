6a35619b75789de06d61085ce6e2e9ea9ffc6fef
henok Gebremedhn <henoklwi@gmail.com>
2020-03-31 15:18:23 -0700

    finished lab-05

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -1,8 +1,8 @@
 'use strict';
 
 class Node {
-  constructor(value) {
-    this.val = value;
+  constructor(val) {
+    this.val = val;
     this.next = null;
     this.prev = null;
   }
@@ -10,32 +10,53 @@ class Node {
 
 
 
+
 class LinkedList {
-  constructor(value) {
-    this.head = value;
+  constructor() {
+    this.head = null;
   }
 
   insert(val) {
     let newNode = new Node(val);
     newNode.next = this.head;
     this.head = newNode;
+    console.log(newNode);
   }
   includes(val) {
-    if (this.head === val) {
-      false;
-    } else true;
+    let current = this.head;
+    while (current) {
+      if (current.val === val) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   }
   toString() {
-    while (this.head) {
-      console.log('{' + this.head + '}' + '->' + '{' + this.head + '}' + '->' + '{' + this.head + '}' + '->' + 'NULL');
-      this.head = this.head.next;
+    let current = this.head;
+    console.log(current);
+    let str = '';
+    while (current) {
+
+      str += '{' + current.val + '}' + '->';
+      current = current.next;
     }
+    str += 'NULL';
+    console.log(str);
   }
 }
+// let node1 = new Node();
+// let node2 = new Node(9);
 
-let myNode = new LinkedList(3);
+let myNode = new LinkedList();
 
-myNode.insert('A');
-myNode.insert('B');
-myNode.insert('C');
+
+myNode.insert(2);
+myNode.insert(5);
+myNode.insert(7);
+
+
 myNode.toString();
+console.log(myNode.includes(5));
+
+module.exports = LinkedList;
