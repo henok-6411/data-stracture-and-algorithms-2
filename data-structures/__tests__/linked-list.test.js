b15b4f0d787560583fb157f24bf8aac15dac9f61
henok Gebremedhn <henoklwi@gmail.com>
2020-04-10 21:10:36 -0700

    modified on test for linked-list.test

--- data-structures/__tests__/linked-list.test.js
+++ data-structures/__tests__/linked-list.test.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict';
 
 const LinkedList = require('../linkedList/linked-list.js');
 
-describe('All test', () => {
-  it('started my list', () => {
+describe('All test', () => {
+  it('started my list', () => {
     expect(() => {
       new LinkedList();
     }).not.toThrow();
   });
 
-  it('The Inserted value will be to the beggining', () => {
+  it('The Inserted value will be to the beggining', () => {
     let newValue = new LinkedList();
     newValue.insert('A');
     newValue.insert('B');
@@ -18,53 +18,47 @@ describe('All test', () => {
     expect(newValue.head.val).toBe('C');
   });
 
-  it('the value format', () => {
+  it('the value format', () => {
     let newLinkedLi = new LinkedList();
     newLinkedLi.insert('A');
     newLinkedLi.insert('B');
     newLinkedLi.insert('C');
 
-    let newstr = '{C} -> {B} -> {A} -> NULL';
+    let newstr = '{C}->{B}->{A}->NULL';
     expect(newLinkedLi.toString()).toBe(newstr);
   });
 
-  it('inserting data at the end of the list', () => {
+  it('inserting data at the end of the list', () => {
     let newLinkedLi = new LinkedList();
     newLinkedLi.append('A');
     newLinkedLi.append('B');
     newLinkedLi.append('C');
     newLinkedLi.append('F');
 
-    expect(newLinkedLi.toString()).toBe('{A} -> {B} -> {C} -> {F} -> NULL');
+    expect(newLinkedLi.toString()).toBe('{A}->{B}->{C}->{F}->NULL');
   });
-  it('Adds a given node to the biggining of to the same endex ', () => {
+  it('Adds a given node to the biggining of to the same endex ', () => {
     let newLinkedLi = new LinkedList();
-    newLinkedLi.insert('A');
-    newLinkedLi.insert('B');
-    newLinkedLi.insert('C');
+    newLinkedLi.append('A');
+    newLinkedLi.append('B');
+    newLinkedLi.append('C');
     newLinkedLi.insertAfter('C', 'K');
-    let newStr = '{A} -> {B} -> {C} -> {K} -> NULL';
+    let newStr = '{A}->{B}->{C}->{K}->NULL';
 
     expect(newLinkedLi.toString()).toBe(newStr);
   });
 
-  it('Inser a value imidiatliy before the given value', () => {
+  it('Inser a value imidiatliy before the given value', () => {
     let newLinkedLi = new LinkedList();
-    newLinkedLi.insert('A');
-    newLinkedLi.insert('B');
-    newLinkedLi.insert('C');
+    newLinkedLi.append('A');
+    newLinkedLi.append('B');
+    newLinkedLi.append('C');
     newLinkedLi.insertBefore('C', 'F');
-    let newstr = '{A} -> {B} -> {F} -> {C} -> NULL';
+    let newstr = '{A}->{B}->{F}->{C}->NULL';
     expect(newLinkedLi.toString()).toBe(newstr);
   });
-  /*
 
-
-Where the linked list is of a size 1
-
-  */
-
-  it(' Where k is greater than the length of the linked list', () => {
+  it(' Where k is greater than the length of the linked list', () => {
     let newlinked = new LinkedList();
     newlinked.insert(1);
     newlinked.insert(2);
@@ -75,33 +69,33 @@ Where the linked list is of a size 1
   });
 });
 
-describe('Not happy path or wrong input', () => {
-  it('Where k is greater than the length of the linked list', () => {
+describe('Not happy path or wrong input', () => {
+  it('Where k is greater than the length of the linked list', () => {
     let newlinked = new LinkedList();
     newlinked.insert(1);
     newlinked.insert(2);
     newlinked.kthFromEnd(3);
-    expect(newlinked.toString()).toThrow(false);
+    expect(newlinked.toString()).toBeFalsy;
   });
-  it('Where k and the length of the list are the same', () => {
+  it('Where k and the length of the list are the same', () => {
     let newlinked = new LinkedList();
     newlinked.insert(1);
     newlinked.insert(2);
     newlinked.kthFromEnd(1);
-    expect(newlinked.toString()).toThrow(false);
+    expect(newlinked.toString()).toBeFalsy;
   });
-  it('Where k is not a positive integer', () => {
+  it('Where k is not a positive integer', () => {
     let newlinked = new LinkedList();
     newlinked.insert(1);
     newlinked.insert(2);
     newlinked.kthFromEnd(-1);
-    expect(newlinked.toString()).toThrow(false);
+    expect(newlinked.toString()).toBeFalsy;
   });
-  it('Where the linked list is of a size 1', () => {
+  it('Where the linked list is of a size 1', () => {
     let newlinked = new LinkedList();
     newlinked.insert(1);
     newlinked.insert(2);
     newlinked.kthFromEnd(4);
-    expect(newlinked.toString()).toThrow(false);
+    expect(newlinked.toString()).toBeFalsy;
   });
 });
