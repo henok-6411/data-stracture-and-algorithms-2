c67a05cb4b7e28657a443cdcbd0c509acaa04f8d
henok Gebremedhn <henoklwi@gmail.com>
2020-04-26 15:42:24 -0700

    working on fifo-animal.test

--- fifoAnimalShelter/__test__/fifo-animal-shelter.test.js
+++ fifoAnimalShelter/__test__/fifo-animal-shelter.test.js
@@ -14,25 +14,31 @@ describe('The happy path of animal shelter', () => {
   });
   it('It can seccessfully dequeue a cat from the shelter', () => {
     let newAnimal = new AnimalShelte();
-    let deqCat = new Animals.Cat('cat');
-    newAnimal.enqueue('dog1');
-    newAnimal.enqueue('dog2');
-    newAnimal.enqueue('cat');
-    newAnimal.enqueue('dog3');
+    let endDog1 = new Animals.Dog('dog1');
+    let endDog2 = new Animals.Dog('dog2');
+    let endCat = new Animals.Cat('cat');
+    let endDog3 = new Animals.Dog('dog3');
+
+    newAnimal.enqueue(endDog1);
+    newAnimal.enqueue(endDog2);
+    newAnimal.enqueue(endCat);
+    newAnimal.enqueue(endDog3);
     expect(newAnimal.isEmpty()).toBe(false);
-    expect(newAnimal.dequeue(deqCat)).toBe('cat');
+    let dequeAnimal = newAnimal.dequeue('cat');
+    console.log(dequeAnimal.val);
+    expect(dequeAnimal.val.species).toBe('cat');
   });
   it('It can successfully dequeue a dog from the shelter', () => {
     let newAnimal = new AnimalShelte();
     let deqDog = new Animals.Dog('dog');
-    console.log(deqDog);
+    // console.log(deqDog);
     newAnimal.enqueue('cat1');
     newAnimal.enqueue('dog');
     newAnimal.enqueue('cat2');
     newAnimal.enqueue('cat3');
-    console.log(newAnimal);
+    // console.log(newAnimal);
     expect(newAnimal.isEmpty()).toBe(false);
 
-    expect(newAnimal.dequeue(deqDog)).toBe('dog');
+    expect(newAnimal.dequeue('dog')).toBe('dog');
   });
 });
