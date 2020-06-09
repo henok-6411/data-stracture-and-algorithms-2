2ece42797851737f0abb3e5a99da0ac283a06d46
henok Gebremedhn <henoklwi@gmail.com>
2020-06-08 18:15:52 -0700

    finished working on insert sort.js

--- data-structures/__tests__/insertSort.test.js
+++ data-structures/__tests__/insertSort.test.js
@@ -1,16 +1,16 @@
 'use strict';
-const sort = require('../../insertSort/insertSort.js');
+const selectionSort = require('../../insertSort/insertSort.js');
 
 describe('Happy path of sorted array' , () => {
   it('It successfully sort array' , () =>{
     let array = [8,9,1,2,5];
-    expect(sort(array)).toBe([1,2,5,8,9]);
+    expect(selectionSort(array)).toEqual([1,2,5,8,9]);
   });
 });
 
 describe('Edge cases' , () =>{
   it('It successfully accept negative number' , () =>{
     let array = [8,9,1,-3,-9];
-    expect(sort(array)).toBe([-9,-3,1,8,9]);
+    expect(selectionSort(array)).toEqual([-9,-3,1,8,9]);
   });
 });
