7972841a18af3b3c5880315b1a8ff0592f9ca89d
henok Gebremedhn <henoklwi@gmail.com>
2020-06-26 13:22:55 -0700

    modified readme

--- repeated-word/repeated-word.js
+++ repeated-word/repeated-word.js
@@ -11,14 +11,13 @@ const repeatedWord = (str) => {
 
 let hashValue = Object.values(hashTable);
 let key = Object.keys(hashTable);
-let notFound = 'There are no duplicated words';
+let notFound = 'There are no duplicated words!';
 
 
 for(let j = 0; j < key.length; j++){
   if(hashValue[j] > 1){
     return key[j];
   } 
-  
 }
 return notFound;
 
